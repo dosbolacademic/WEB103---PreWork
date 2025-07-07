a7066e6cecc9c8c49cd6d8dea6291f2247c9ec20
Dosbol Aliev <50006912+dosbolacademic@users.noreply.github.com>
2025-07-06 23:09:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { useRoutes } from 'react-router-dom';  // Note the curly braces!
+import { BrowserRouter, useRoutes } from 'react-router-dom';  // import BrowserRouter here
 import ShowCreators from './pages/ShowCreators';
 import ViewCreator from './pages/ViewCreator';
 import EditCreator from './pages/EditCreator';
 import AddCreator from './pages/AddCreator';
+import NotFound from './pages/NotFound';
 import './App.css';
 
-function App() {
+function AppRoutes() {
   const routes = useRoutes([
     { path: '/', element: <ShowCreators /> },
     { path: '/creators/:id', element: <ViewCreator /> },
     { path: '/edit/:id', element: <EditCreator /> },
     { path: '/add', element: <AddCreator /> },
+    { path: '*', element: <NotFound /> }
   ]);
+  return routes;
+}
 
+function App() {
   return (
-    <div className="App">
-      {routes}
-    </div>
+    <BrowserRouter basename="/WEB103---PreWork">
+      <div className="App">
+        <AppRoutes />
+      </div>
+    </BrowserRouter>
   );
 }
 
